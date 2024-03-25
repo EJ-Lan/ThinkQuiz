5eb95bc9a688e9d0ee502e043006ef040fb16af7
EJ-Lan <ervinjon_lanada@outlook.com>
2024-03-25 01:17:14 -0600

    Set up routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cardRoutes = require('./routes/card');
+const deckRoutes = require('./routes/deck')
 
 // express app
 const app = express()
@@ -12,6 +14,10 @@ app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
 })
+
+// routes
+app.use('/api/cards', cardRoutes)
+app.use('/api/decks', deckRoutes)
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI)
