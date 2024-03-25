51043034c50d07d9874324e18cd13fc5a49fe997
EJ-Lan <ervinjon_lanada@outlook.com>
2024-03-25 01:02:05 -0600

    Connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ app.use((req, res, next) => {
     console.log(req.path, req.method)
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`);
-});
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`connected to db & listening on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
