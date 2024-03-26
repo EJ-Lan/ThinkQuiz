581729913f9c8d632da93bce7a4ea527246ccb5c
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-03-25 19:43:32 -0600

    Add linking and componenets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
