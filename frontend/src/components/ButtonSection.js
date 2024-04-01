c04d9a934985b231892401d8cfe898c5aa0eba20
EJ-Lan <ervinjon_lanada@outlook.com>
2024-04-01 17:17:36 -0600

    remove home button

--- frontend/src/components/ButtonSection.js
+++ frontend/src/components/ButtonSection.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 const ButtonSection = ({ deckId }) => {
     return (
         <div className="buttons-section">
-            <Link to="/" className="button">Home</Link>
             <Link to={`/decks/${deckId}/quiz`} className="button">Quiz</Link>
         </div>
     );
