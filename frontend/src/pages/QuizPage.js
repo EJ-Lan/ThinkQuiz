2921644ac1519972fde1a6c76f7f9987fa8f9171
EJ-Lan <ervinjon_lanada@outlook.com>
2024-04-01 16:32:52 -0600

    Fix context issue

--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -1,17 +1,19 @@
 import { useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { DecksContext } from '../context/DecksContext';
+import { CardsContext } from '../context/CardsContext'; 
 import QuizCards from '../components/QuizCards';
 import QuizButtons from '../components/QuizButtons';
 
 const QuizPage = () => {
     const { deckId } = useParams();
     const { decks } = useContext(DecksContext);
+    const { cards: allCards } = useContext(CardsContext); 
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
     const [showAnswer, setShowAnswer] = useState(false);
 
     const deck = decks ? decks.find(deck => deck._id === deckId) : null;
-    const cards = deck ? deck.cards : [];
+    const cards = deck ? allCards.filter(card => card.deck === deckId) : []; 
 
     return (
         <div>
